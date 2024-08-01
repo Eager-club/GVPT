9f4d232f4384192e5ae3dc779281e3958e32e3e9
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-08-01 16:36:25 +0530

    updated

--- templates/includeHTML.js
+++ templates/includeHTML.js
@@ -1,5 +1,24 @@
-fetch('./templates/nav.html')
-              .then(response => response.text())
-              .then(data => {
-                  document.getElementById('nav-placeholder').innerHTML = data;
-              });
+  // Fetch and insert the nav template
+  fetch('./templates/nav.html')
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById('nav-placeholder').innerHTML = data;
+
+      // Reinitialize sticky functionality
+      initializeStickyNav();
+  });
+
+// Function to initialize sticky navigation
+function initializeStickyNav() {
+  window.addEventListener('scroll', function() {
+      var mainNav = document.querySelector('.main-nav');
+      var navLogo = document.querySelector('.logo');
+      if (window.scrollY > 170) { // Adjust the scroll value as needed
+          mainNav.classList.add('sticky');
+          navLogo.classList.add('sticky');
+      } else {
+          mainNav.classList.remove('sticky');
+          navLogo.classList.remove('sticky');
+      }
+  });
+}
