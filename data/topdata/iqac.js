5ea56c0aea80a71e99dbd3d348ffe2cfc1330dc7
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-08-07 15:41:37 +0530

    iqac updated

---
+++ data/topdata/iqac.js
@@ -0,0 +1,42 @@
+const iqacData = {
+        sub1:{
+            heading:'IQAC MINUTES & ATR REPORTS',
+            files:[
+                {text: 'ATR REPORT 2022 - 2023',link: '#'},
+                {text: 'ATR REPORT 2021 - 2022',link: '#'},
+                {text: 'ATR REPORT 2020 - 2021',link: '#'},
+                {text: 'ATR REPORT 2019 - 2020',link: '#'},
+                {text: 'ATR REPORT 2018 - 2019',link: '#'},
+                {text: 'ATR REPORT 2017 - 2018',link: '#'},
+                {text: 'ATR REPORT 2016 - 2017',link: '#'},
+                {text: 'ATR REPORT 2015 - 2016',link: '#'},
+                {text: 'ATR REPORT 2014 - 2015',link: '#'},
+                {text: 'ATR REPORT 2013 - 2014',link: '#'}
+                
+            ]
+        },
+        sub2:{
+            heading:'ANNUAL REPORTS',
+            files:[
+                {text: 'ANNUAL REPORT 2022-2023',link: '#'},
+                {text: 'ANNUAL REPORT 2021-2022',link: '#'},
+                {text: 'ANNUAL REPORT 2020-2021',link: '#'},
+                {text: 'ANNUAL REPORT 2019-2020',link: '#'},
+                {text: 'ANNUAL REPORT 2018-2019',link: '#'},
+                {text: 'ANNUAL REPORT 2014-2018',link: '#'}
+                
+            ]
+        },
+        sub3:{
+            heading:'ACADEMIC AUDIT REPORTS',
+            files:[
+                {text: 'ACADEMIC AUDIT 2022-2023',link: '#'},
+                {text: 'ACADEMIC AUDIT 2022-2023',link: '#'},
+                {text: 'ACADEMIC AUDIT 2022-2023',link: '#'},
+                {text: 'ACADEMIC AUDIT 2022-2023',link: '#'},
+                {text: 'ACADEMIC AUDIT 2022-2023',link: '#'}
+            ]
+        }
+    }
+
+export default iqacData;
