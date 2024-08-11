a461fc4f8a79b7ade0a18124fccfa0552a5ef8a7
Kavuri13 <kavuri.bhavyasri2004@gmail.com>
2024-08-11 11:08:03 +0530

    onlinepayment

---
+++ top/data.js
@@ -0,0 +1,19 @@
+const data = {
+    onlineFeePayment: [
+        { title: "DEGREE BSC FEE PAYMENT", link: "#" },
+        { title: "BBA,BCA,BCA(DATA SCIENCE)", link: "#" },
+        { title: "MSC & MHRM FEE PAYMENT", link: "#" },
+        { title: "MBA FEE PAYMENT", link: "#" },
+        { title: "MCA FEE PAYMENT", link: "#" },
+        { title: "ENGINEERING FEE PAYMENT", link: "#" },
+        { title: "TRANSPORT FEE PAYMENT", link: "#" }
+    ],
+    feeStructure: [
+        { course: "M.B.A", fee: "70,000/-" },
+        { course: "M.C.A", fee: "70,000/-" },
+        { course: "MTECH STRUCTURAL ENGINEERING", fee: "63,000/-" },
+        { course: "BTECH CIVIL ENGINEERING", fee: "63,000/-" },
+        { course: "BTECH COMPUTER SCIENCE ENGINEERING", fee: "63,000/-" },
+        { course: "BTECH ELECTRONICS AND COMMUNICATION ENGINEERING", fee: "63,000/-" }
+    ]
+};
