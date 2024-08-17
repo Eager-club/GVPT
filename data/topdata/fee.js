25a2950c0b0e627d822752d387147e166d0baeed
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-08-17 21:09:06 +0530

    updated

---
+++ data/topdata/fee.js
@@ -0,0 +1,28 @@
+const feePaymentData = {
+    sub1: {
+        heading: 'ONLINE FEE PAYMENT',
+        files: [
+            { text: 'DEGREE BSC FEE PAYMENT', link: '#' },
+            { text: 'BBA, BCA, BCA (DATA SCIENCE)', link: '#' },
+            { text: 'MSC & MHRM FEE PAYMENT', link: '#' },
+            { text: 'MBA FEE PAYMENT', link: '#' },
+            { text: 'MCA FEE PAYMENT', link: '#' },
+            { text: 'ENGINEERING FEE PAYMENT', link: '#' },
+            { text: 'TRANSPORT FEE PAYMENT', link: '#' }
+        ]
+    },
+    sub2: {
+        heading: 'FEE STRUCTURE',
+        files: [
+            { course: 'M.B.A', fee: '70,000/-' },
+            { course: 'M.C.A', fee: '70,000/-' },
+            { course: 'STRUCTURAL ENGINEERING (MTECH)', fee: '63,000/-' },
+            { course: 'CIVIL ENGINEERING (MTECH)', fee: '63,000/-' },
+            { course: 'CIVIL ENGINEERING (BTECH)', fee: '63,000/-' },
+            { course: 'COMPUTER SCIENCE ENGINEERING (BTECH)', fee: '63,000/-' },
+            { course: 'ELECTRONICS AND COMMUNICATION ENGINEERING (BTECH)', fee: '63,000/-' }
+        ]
+    }
+};
+
+export default feePaymentData;
