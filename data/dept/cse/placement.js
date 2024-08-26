9e1170093998b03215fa453a583e70043a604012
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-08-26 21:52:20 +0530

    chore: Update infrastructure and syllabus sections in cse.html, fix indentation in faculty.html and index.html, and update CSS styles

---
+++ data/dept/cse/placement.js
@@ -0,0 +1,17 @@
+const Placements = {
+    Placements: [
+        { text: "Placements-2021-22", link: "URL_" },
+        { text: "Placements-2020-21", link: "URL_" },
+        { text: "Placements-2019-20", link: "URL_" },
+        { text: "Placements-2018-19", link: "URL_" },
+        { text: "Placements-2017-18", link: "URL_" },
+        { text: "Placements-2016-17", link: "URL_" },
+    ],
+    "Higher Studies of Students": [
+        { text: "Higher Studies of Batch- 2016-20", link: "URL_" },
+        { text: "Higher Studies of Batch- 2015-19", link: "URL_" },
+        { text: "Higher Studies of Batch- 2014-18", link: "URL_" },
+    ]
+};
+
+export default Placements;
