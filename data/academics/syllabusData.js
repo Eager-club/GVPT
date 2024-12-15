9d270e7a6999a806d79b6f0d4f98feb3685f0231
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-12-15 17:00:01 +0530

    Add academic calendar and syllabus data for various programs

---
+++ data/academics/syllabusData.js
@@ -0,0 +1,29 @@
+const syllabusData = {
+    postGraduatePrograms: [
+        { text: "MBA - Master of Business Administration Syllabus", link: "URL_TO_MBA_SYLLABUS" },
+        { text: "MCA - Master of Computer Applications Syllabus", link: "URL_TO_MCA_SYLLABUS" },
+        { text: "M.Sc. (Computer Science) Syllabus", link: "URL_TO_MSC_CS_SYLLABUS" },
+        { text: "MSc Organic Chemistry Syllabus", link: "URL_TO_MSC_OC_SYLLABUS" },
+        { text: "Master of Human Resource Management (MHRM) Syllabus", link: "URL_TO_MHRM_SYLLABUS" }
+    ],
+    undergraduateEngineeringPrograms: [
+        { text: "Engineering & Technology Civil Engineering Syllabus", link: "URL_TO_CIVIL_ENGINEERING_SYLLABUS" },
+        { text: "Engineering & Technology Computer Science & Engineering Syllabus", link: "URL_TO_CSE_SYLLABUS" },
+        { text: "Engineering & Technology Computer Science & Engineering (Artificial Intelligence & Machine Learning) Syllabus", link: "URL_TO_CSE_AIML_SYLLABUS" },
+        { text: "Engineering & Technology Electronics and Communication Engineering Syllabus", link: "URL_TO_ECE_SYLLABUS" },
+        { text: "Engineering & Technology Mechanical Engineering Syllabus", link: "URL_TO_MECHANICAL_ENGINEERING_SYLLABUS" }
+    ],
+    undergraduatePrograms: [
+        { text: "B.Sc. Mathematics, Physics and Computer Science Syllabus", link: "URL_TO_BSC_MATHS_PHYSICS_CS_SYLLABUS" },
+        { text: "B.Sc. Mathematics, Electronics and Computer Science Syllabus", link: "URL_TO_BSC_MATHS_ELEC_CS_SYLLABUS" },
+        { text: "B.Sc. Mathematics, Statistics and Computer Science Syllabus", link: "URL_TO_BSC_MATHS_STATS_CS_SYLLABUS" },
+        { text: "B.Sc. Mathematics, Physics and Chemistry Syllabus", link: "URL_TO_BSC_MATHS_PHYSICS_CHEM_SYLLABUS" },
+        { text: "B.Sc. Microbiology, Biotechnology and Biochemistry Syllabus", link: "URL_TO_BSC_MICRO_BIO_TECH_BIOCHEM_SYLLABUS" },
+        { text: "Bachelor of Business Administration Syllabus", link: "URL_TO_BBA_SYLLABUS" },
+        { text: "Bachelor of Computer Applications Syllabus", link: "URL_TO_BCA_SYLLABUS" },
+        { text: "B.Com. (Computers) Syllabus", link: "URL_TO_BCOM_COMPUTERS_SYLLABUS" },
+        { text: "B.Com. Syllabus", link: "URL_TO_BCOM_SYLLABUS" },
+        { text: "B.Vocational. Syllabus", link: "URL_TO_BVOC_SYLLABUS" }
+    ]
+};
+export default syllabusData;
